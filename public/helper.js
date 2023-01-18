ba817a1a822d3de156c218233b9b2a3b593fedd4
LisadiKaprio <lisadikaprio@gmail.com>
2023-01-18 20:41:24 +0100

    move script to separate file

--- public/helper.js
+++ public/helper.js
@@ -1,9 +1,24 @@
-window.addEventListener('message', (event) => {
-  console.log(event);
-});
-//debugger;
-//const local = window.frameElement.attributes.getNamedItem('helperUrl');
-//const online = 'https://extension-files.twitch.tv/helper/v1/twitch-ext.min.js';
-//const el = document.createElement('script');
-//el.src = local ? local.value : online;
-//document.head.appendChild(el);
+
+var countUpDate = new Date("January 17, 2021 21:00:00").getTime();
+var x = setInterval(updateTimer, 1000);
+var oneSecond = 1000;
+var oneMinute = oneSecond * 60;
+var oneHour = oneMinute * 60;
+var oneDay = oneHour * 24;
+
+function updateTimer() {
+  var now = new Date().getTime();
+  var distance = now - countUpDate;
+  var time = {
+    days: Math.floor(distance / oneDay),
+    hours: Math.floor((distance % oneDay) / oneHour),
+    minutes: Math.floor((distance % oneHour) / oneMinute),
+    seconds: Math.floor((distance % oneMinute) / oneSecond)
+  }
+
+  time.hours = time.hours < 10 ? "0" + time.hours : time.hours;
+  time.minutes = time.minutes < 10 ? "0" + time.minutes : time.minutes;
+  time.seconds = time.seconds < 10 ? "0" + time.seconds : time.seconds;
+
+  document.getElementById("timer").innerHTML = `${time.days} days ${time.hours}:${time.minutes}:${time.seconds}`;
+}
