2b2585913a6cdef8d7bdda24e3a85f9387556110
LisadiKaprio <lisadikaprio@gmail.com>
2024-02-19 23:43:14 +0100

    update: Navalny was murdered

--- public/helper.js
+++ public/helper.js
@@ -1,14 +1,13 @@
 
 var countUpDate = new Date("January 17, 2021 21:00:00").getTime();
-var x = setInterval(updateTimer, 1000);
+var murderDate = new Date("February 16, 2024 09:17:00").getTime();
 var oneSecond = 1000;
 var oneMinute = oneSecond * 60;
 var oneHour = oneMinute * 60;
 var oneDay = oneHour * 24;
 
 function updateTimer() {
-  var now = new Date().getTime();
-  var distance = now - countUpDate;
+  var distance = murderDate - countUpDate;
   var time = {
     days: Math.floor(distance / oneDay),
     hours: Math.floor((distance % oneDay) / oneHour),
@@ -20,5 +19,10 @@ function updateTimer() {
   time.minutes = time.minutes < 10 ? "0" + time.minutes : time.minutes;
   time.seconds = time.seconds < 10 ? "0" + time.seconds : time.seconds;
 
+
+  console.log(countUpDate);
+
   document.getElementById("timer").innerHTML = `${time.days} days ${time.hours}:${time.minutes}:${time.seconds}`;
 }
+
+updateTimer();
